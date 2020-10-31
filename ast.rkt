21e6f7ed647e3b0f5e2fcef858aacb0db818cac7
Michael Flanders <mkf727@cs.washington.edu>
2020-10-30 23:56:51 -0700

    add macros for defining new binops and unops

--- ast.rkt
+++ ast.rkt
@@ -2,6 +2,12 @@
 
 (provide (all-defined-out))
 
+(require (for-syntax racket/syntax)
+         racket/generic)
+
+(define-generics printable-node
+  (node-print printable-node))
+
 ;; Terms
 (struct Term () #:transparent)
 (struct Num Term (val) #:transparent)
@@ -23,41 +29,68 @@
 (struct Arg (type name range) #:transparent)
 (struct Range (low high) #:transparent)
 
-;; Expressions
-(struct Expr () #:transparent)
+; Expressions
+(define-syntax (define-binop stx)
+  (syntax-case stx ()
+    [(_ name fmt-str)
+     (with-syntax ([node-name (format-id #'name "~a" #'name)]
+                   [expr-l (format-id #'name "~a-l" #'name)]
+                   [expr-r (format-id #'name "~a-r" #'name)])
+       #'(struct node-name BinOp (l r)
+           #:transparent
+           #:methods gen:printable-node
+           [(define (node-print node)
+              (format fmt-str
+                      (node-print (expr-l node))
+                      (node-print (expr-r node))))]))]))
+
+(define-syntax (define-unop stx)
+  (syntax-case stx ()
+    [(_ name fmt-str)
+     (with-syntax ([node-name (format-id #'name "~a" #'name)]
+                   [expr-val (format-id #'name "~a-val" #'name)])
+       #'(struct node-name UnOp (val)
+           #:transparent
+           #:methods gen:printable-node
+           [(define (node-print node)
+              (format fmt-str
+                      (node-print (expr-val node))))]))]))
+
+(struct Expr () #:transparent) ; Super classes
 (struct BinOp Expr () #:transparent)
-(struct Add BinOp (l r) #:transparent)
-(struct Minus BinOp (l r) #:transparent)
-(struct Mult BinOp (l r) #:transparent)
-(struct Div BinOp (l r) #:transparent)
-
-(struct Lt BinOp (l r) #:transparent)
-(struct Gt BinOp (l r) #:transparent)
-(struct Lte BinOp (l r) #:transparent)
-(struct Gte BinOp (l r) #:transparent)
-(struct Eq BinOp (l r) #:transparent)
-
-(struct StrContains BinOp (l r) #:transparent)
-(struct StrSplit BinOp (l r) #:transparent)
-(struct StrEquals BinOp (l r) #:transparent)
-(struct StrConcat BinOp (l r) #:transparent)
-(struct StrRef BinOp (l r) #:transparent)
-
-(struct ArrContains BinOp (l r) #:transparent)
-(struct ArrSplit BinOp (l r) #:transparent)
-(struct ArrEquals BinOp (l r) #:transparent)
-(struct ArrConcat BinOp (l r) #:transparent)
-(struct ArrRef BinOp (l r) #:transparent)
-
 (struct UnOp Expr () #:transparent)
-(struct String UnOp (contents) #:transparent)
-(struct Array UnOp (contents) #:transparent)
-(struct Not UnOp (val) #:transparent)
 
-(struct StrLen UnOp (str) #:transparent)
-(struct StrEmpty UnOp (str) #:transparent)
-(struct ArrLen UnOp (str) #:transparent)
-(struct ArrEmpty UnOp (str) #:transparent)
+(define-binop Add "(~a + ~a")
+(define-binop Mult "(~a * ~a")
+(define-binop Minus "(~a - ~a")
+(define-binop Div "(~a / ~a")
+
+(define-binop Lt "(~a < ~a")
+(define-binop Gt "(~a > ~a")
+(define-binop Lte "(~a <= ~a")
+(define-binop Gte "(~a >= ~a")
+(define-binop Eq "(~a == ~a")
+
+(define-binop StrContains "(string-contains? ~a ~a)")
+(define-binop StrSplit "(string-split ~a ~a)")
+(define-binop StrEquals "(string-equals? ~a ~a)")
+(define-binop StrConcat "(string-concat ~a ~a)")
+(define-binop StrRef "(string-ref ~a ~a)")
+
+(define-binop ArrContains "(array-contains? ~a ~a)")
+(define-binop ArrSplit "(array-split ~a ~a)")
+(define-binop ArrEquals "(array-equals? ~a ~a)")
+(define-binop ArrConcat "(array-concat ~a ~a)")
+(define-binop ArrRef "(array-ref ~a ~a)")
+
+(define-unop String "\"~a\"")
+(define-unop Array "[~a]")
+(define-unop Not "!~a")
+
+(define-unop StrLen "(string-len ~a)")
+(define-unop StrEmpty "(string-empty? ~a)")
+(define-unop ArrLen "(array-len ~a)")
+(define-unop ArrEmpty "(array-empty? ~a)")
 
 ;; Helper functions
 (define operators (list "+" "-" "*" "/"))
