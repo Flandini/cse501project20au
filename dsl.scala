b5d1b2e024ce89786bda214347db172aa00c105c
Michael Flanders <mkf727@cs.washington.edu>
2020-11-29 20:09:58 -0800

    Building out interp. finished for the night

--- dsl.scala
+++ dsl.scala
@@ -7,40 +7,45 @@ object AST {
     case object StrIterType extends Type
 
     sealed trait Expr extends ForBody
+    sealed trait StrExpr extends Expr
+    sealed trait NumExpr extends Expr
+    sealed trait IterExpr extends Expr
     // Numeric ops
-    case class Add(left: Expr, right: Expr) extends Expr
-    case class Minus(left: Expr, right: Expr) extends Expr
-    case class Mult(left: Expr, right: Expr) extends Expr
-    case class Div(left: Expr, right: Expr) extends Expr
-    case class Eq(left: Expr, right: Expr) extends Expr
-    case class Neq(left: Expr, right: Expr) extends Expr
-    case class Gte(left: Expr, right: Expr) extends Expr
-    case class Lte(left: Expr, right: Expr) extends Expr
-    case class Gt(left: Expr, right: Expr) extends Expr
-    case class Lt(left: Expr, right: Expr) extends Expr
-    case class Or(left: Expr, right: Expr) extends Expr
-    case class And(left: Expr, right: Expr) extends Expr
-    case class Not(left: Expr) extends Expr
+    case class Add(left: Expr, right: Expr) extends NumExpr
+    case class Minus(left: Expr, right: Expr) extends NumExpr
+    case class Mult(left: Expr, right: Expr) extends NumExpr
+    case class Div(left: Expr, right: Expr) extends NumExpr
+    case class Eq(left: Expr, right: Expr) extends NumExpr
+    case class Neq(left: Expr, right: Expr) extends NumExpr
+    case class Gte(left: Expr, right: Expr) extends NumExpr
+    case class Lte(left: Expr, right: Expr) extends NumExpr
+    case class Gt(left: Expr, right: Expr) extends NumExpr
+    case class Lt(left: Expr, right: Expr) extends NumExpr
+    case class Or(left: Expr, right: Expr) extends NumExpr
+    case class And(left: Expr, right: Expr) extends NumExpr
+    case class Not(left: Expr) extends NumExpr
+
+    case class FunCall(params: List[Expr]) extends Expr
 
     // String ops
-    case class Ston(left: Expr) extends Expr
-    case class StrSplit(left: Expr, right: Expr) extends Expr
-    case class StrEquals(left: Expr, right: Expr) extends Expr
-    case class StrConcat(left: Expr, right: Expr) extends Expr
-    case class StrAppend(left: Expr, right: Expr) extends Expr
-    case class StrLength(left: Expr) extends Expr
+    case class Ston(left: Expr) extends StrExpr
+    case class StrSplit(left: Expr, right: Expr) extends StrExpr
+    case class StrEquals(left: Expr, right: Expr) extends StrExpr
+    case class StrConcat(left: Expr, right: Expr) extends StrExpr
+    case class StrAppend(left: Expr, right: Expr) extends StrExpr
+    case class StrLength(left: Expr) extends StrExpr
 
     // iter ops
-    case class IterConcat(left: Expr, right: Expr) extends Expr
-    case class IterFirst(left: Expr) extends Expr
-    case class IterLength(left: Expr) extends Expr
+    case class IterConcat(left: Expr, right: Expr) extends IterExpr
+    case class IterFirst(left: Expr) extends IterExpr
+    case class IterLength(left: Expr) extends IterExpr
 
-    // Smallest exprs, Term
-    case class Var(name: String) extends Expr
-    case class IntLit(num: Int, width: Int, signed: Boolean) extends Expr
-    case class StrLit(value: String, range: Option[Range]) extends Expr
-    case class IntIter(value: List[IntLit], range: Option[Range]) extends Expr
-    case class StrIter(value: List[StrLit], range: Option[Range]) extends Expr
+    sealed trait Term extends Expr
+    case class Var(name: String) extends Term
+    case class IntLit(num: Int, width: Int, signed: Boolean) extends Term
+    case class StrLit(value: String, range: Option[Range]) extends Term
+    case class IntIter(value: List[IntLit], range: Option[Range]) extends Term
+    case class StrIter(value: List[StrLit], range: Option[Range]) extends Term
 
     sealed trait Statement extends ForBody
     case class Decl(t: Type, name: String, expr: Option[Expr]) extends Statement
@@ -57,7 +62,7 @@ object AST {
                    subrange: Option[Range],
                    range: Option[Range],
                    name: String)
-
+    case class Error(msg: String)
 
     val dimacs_scanner: FuncDecl =
         FuncDecl(
--- interp.scala
+++ interp.scala
@@ -8,7 +8,6 @@ object idenM {
 }
 
 object Interpreter {
-
     type Name = String
     type Environment = List[(Name, Expr)]
 
@@ -18,8 +17,36 @@ object Interpreter {
     }
 
     def interpFunc(func: FuncDecl, env: Environment): M[Expr] = {
+
     }
 
-    def interpStmt(stmt: Statement, env: Environment): M[Expr] = {
+    def interpStmt(stmt: Statement, env: Environment): Environment = {
     }
+
+    def interpExpr(exp: Expr, env: Environment): M[Expr] = 
+        exp match {
+            case nume: NumExpr => 
+            case stre: StrExpr =>
+            case itere: IterExpr =>
+            case term: Term => 
+            case FunCall(params) => 
+        }
+
+    def interpNumExpr(nume: Expr, env: Environment): M[Expr] = {
+
+    }
+
+    def interpStrExpr(stre: Expr, env: Environment): M[Expr] = {
+
+    }
+
+    def lookup(name: Name, env: Environment): M[Expr] =
+        env match {
+            case Nil => unit(Error(s"Couldn't find var: ${name}"))
+            case (n, v) :: xs => 
+                if (n == name)
+                    unit(v)
+                else
+                    lookup(name, xs)
+        }
 }
