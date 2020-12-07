265b4ed0002bcc6eb7b3a0f80cf69fc96a1839d7
Michael Flanders <mkf727@cs.washington.edu>
2020-12-07 15:10:00 -0800

    Working on symbolic execution lib/runtime

--- symbolic-execution.rkt
+++ symbolic-execution.rkt
@@ -4,12 +4,7 @@
 
 (require "ast.rkt")
 
-;; 1st pass - add assertions that each arg is 
-;; within their specified ranges. assert that
-;; some function-return variable is within range
-
-(define (zero-extend val numzeroes)
-    (concat (bv 0 numzeroes) val))
+;; from https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/z3prefix.pdf
 
 (define-syntax-rule (bvone? x)
     (bveq x (bv 1 1)))
@@ -17,20 +12,48 @@
 (define (check-unsigned-add x y)
     (let ([ext-x (zero-extend x 1)]
           [ext-y (zero-extend y 1)])
-      (assert 
-        (not (bvzero? (msb (bvadd ext-x ext-y)))))))
+        (bvzero? (msb (bvadd ext-x ext-y)))))
 
 (define (check-signed-add x y)
     (let ([msb-x (msb x)]
           [msb-y (msb y)])
-      (assert
-        (or
-            (=> (and (bvone? msb-x) (bvone? msb-y))
-                (bvone? (msb (bvadd x y))))
-            (=> (and (bvzero? msb-x) (bvzero? msb-y))
-                (bvzero? (msb (bvadd x y))))))))
+        (if (and (bvone? msb-x) (bvone? msb-y))
+            (bvone? (msb (bvadd x y)))
+            (if (and (bvzero? msb-x) (bvzero? msb-y))
+                (bvzero? (msb (bvadd x y)))
+                #t))))
+
 
 (define (check-unsigned-sub x y)
     (assert (bvult x y)))
 
-(define (check-signed-sub x y) void)
+(define (check-signed-sub x y)
+    (let ([msb-x (msb x)]
+          [msb-y (msb y)])
+    (if (and (bvzero? msb-x) (bvone? msb-y))
+        (bvzero? (msb (bvsub x y)))
+        (if (and (bvone? msb-x) (bvzero? msb-y))
+            (bvone? (msb (bvsub x y)))
+            #t))))
+
+(define (bv-len x) (length (bitvector->bits x)))
+(define (extend-for-unsigned-mul x) (zero-extend x (bv-len x)))
+(define (extend-for-signed-mul x) (sign-extend x (bv-len x)))
+(define (check-unsigned-mul x y)
+    (let* ([new-x (extend-for-unsigned-mul x)]
+           [new-y (extend-for-unsigned-mul y)]
+           [highbits (sub1 (bv-len new-x))]
+           [lowbits (bv-len x)])
+        (bvzero? (extract highbits lowbits (bvmul new-x new-y)))))
+        
+
+(define-symbolic c d (bitvector 8))
+(assert (bveq c (bv 3 8)))
+(assert (bveq d (bv 24 8)))
+(define cex (verify #|#:assume (assert 
+                                (and (bveq c (bv 3 8))
+                                     (bveq d (bv 15 8))))|#
+                    #:guarantee (assert (check-unsigned-mul c d))))
+(displayln (asserts))
+(displayln cex)
+
